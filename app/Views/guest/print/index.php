3458422a83782e030a94109768b18d912e9670fc
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2025-02-05 16:50:17 +0700

    update print

--- app/Views/guest/print/index.php
+++ app/Views/guest/print/index.php
@@ -1,16 +1,18 @@
-<div class="h-100 w-100" id="content-bg">
+<div class="w-100 vh-100" id="content-bg">
     <div class="mx-5 py-5 h-100">
-        <div class="row text-black h-100">
+        <div class="row text-black">
             <!-- Bagian kiri (SELECT FRAME) -->
-            <div class="col-8 d-flex flex-column">
-                <div class="flex-grow-1 mt-2 d-flex-column justify-content-center align-items-center" style="background-color: green;">
-                <img id="photo" class="img-fluid px-4 py-4" src="<?= BASE_URL ?>assets/photobooth/<?= $dir ?>/my-photo.png">
-                <p class="text-white text-center">THANK YOU</p>
+            <div class="col-8 d-flex flex-column justify-content-center">
+                <div class="mt-2 d-flex justify-content-center align-items-center h-100" style="background-color: green;">
+                    <div class="text-center" style="overflow-y: auto; max-height: 100%;">
+                        <img id="photo" class="img-fluid mt-4" style="max-height: 75vh; object-fit: contain;" src="<?= BASE_URL ?>assets/photobooth/<?= $dir ?>/my-photo.png">
+                        <p class="text-white mt-4 fs-1">- THANK YOU -</p>
+                    </div>
                 </div>
             </div>
             <!-- Bagian kanan (PREVIEW FRAME) -->
             <div class="col-4 d-flex flex-column">
-                <div class="mb-4 flex-grow-1 mt-2 rounded" >
+                <div class="mb-4 flex-grow-1 mt-2 rounded">
                     <div class="d-flex flex-column align-items-center justify-content-center h-100">
                         <div class="bg-white p-4 mb-2">
                             <?= $qrcode ?>
