59c83a05154cdbb244e0c1c3c99d113eea83024d
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2025-03-05 15:01:51 +0700

    add print

--- app/Views/guest/download/js/_js_index.php
+++ app/Views/guest/download/js/_js_index.php
@@ -17,4 +17,39 @@
             swiper: swiper
         }
     });
+
+    function printImage(url) {
+        console.log(url);
+        
+            var printWindow = window.open('', '_blank', 'width=600,height=600');
+
+            if (!printWindow) {
+                alert("Pop-up terblokir! Izinkan pop-up untuk mencetak.");
+                return;
+            }
+
+            printWindow.document.write(`
+            <html>
+            <head>
+                <title>Print Image</title>
+                <style>
+                    body { margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; height: 100vh; }
+                    @page { size: 4in 6in; margin: 0; } /* Ukuran 4R */
+                    img { width: 4in; height: 6in; object-fit: contain; }
+                </style>
+            </head>
+            <body>
+        `);
+
+            printWindow.document.write(`<img src="${url}"`);
+            printWindow.document.write(`</body></html>`);
+            printWindow.document.close();
+
+            printWindow.focus();
+
+            setTimeout(function() {
+                printWindow.print();
+                printWindow.close();
+            }, 500);
+        }
 </script>
