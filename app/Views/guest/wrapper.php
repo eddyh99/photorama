a44de85f06e6dc372713c3305466802fddfafd86
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2025-03-07 10:37:21 +0700

    add notify alert

--- app/Views/guest/wrapper.php
+++ app/Views/guest/wrapper.php
@@ -1,5 +1,14 @@
 <?php 
 require_once(__DIR__ . '/../admin/layout/header.php');
+echo '<style>
+    /* CSS Kustom untuk Notyf */
+    .notyf__toast {
+    background-color: #d9534f; /* Warna latar belakang kuning */
+    color: white; /* Warna teks putih */
+    border-radius: 8px; /* Sudut yang lebih bulat */
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2); /* Bayangan yang lebih menonjol */
+}
+</style>';
 if (isset($content)) {
     echo view($content);
 }
