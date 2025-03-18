59461aa206124abfa06a15b75e4fcb9c6ce9aef5
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2025-03-18 14:33:32 +0700

    Update index.php

--- app/Views/guest/camera/index.php
+++ app/Views/guest/camera/index.php
@@ -6,10 +6,10 @@
 <div class="h-100 w-100" id="content-bg">
     <div class="mx-5 py-5 h-100">
         <h2 class="text-center">SELECT YOUR CAMERA</h2>
-        <div class="row text-black h-75 mt-4">
+        <div class="row text-black h-75 g-4">
             <!-- Bagian kiri  -->
-            <div class="col-6 d-flex flex-column">
-                <div class="bg-black mb-4 flex-grow-1 d-flex align-items-center justify-content-center">
+            <div class="col-lg-6 col-md-12">
+                <div class="bg-black mb-4 d-flex align-items-center justify-content-center">
                     <!-- Kamera 1 -->
                     <video id="camera1" width="100%" height="100%" autoplay></video>
                 </div>
@@ -18,8 +18,8 @@
                 </div>
             </div>
             <!-- Bagian kanan -->
-            <div class="col-6 d-flex flex-column">
-                <div class="bg-black mb-4 flex-grow-1 d-flex align-items-center justify-content-center">
+            <div class="col-lg-6 col-md-12">
+                <div class="bg-black mb-4 d-flex align-items-center justify-content-center">
                     <!-- Kamera 2 -->
                     <video id="camera2" width="100%" height="100%" autoplay></video>
                 </div>
